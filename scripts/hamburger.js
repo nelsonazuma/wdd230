210b21ba4378c7be29129b1dfa200416b0ef182d
Nelson <nelsonazumah@gmail.com>
2023-09-28 23:01:18 +0100

    Add files via upload

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,20 +1,17 @@
-// JavaScript for toggling the mobile menu
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuItems = document.getElementById("menu-items");
+  // Mobile menu toggle
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuItems = document.getElementById("menu-items");
 
-    menuToggle.addEventListener("click", function () {
-        menuItems.classList.toggle("show-menu");
-    });
-});
+  menuToggle.addEventListener("click", function () {
+    menuItems.classList.toggle("show-menu");
+  });
 
+  // Dark mode toggle
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const body = document.body;
 
-// JavaScript for toggling dark mode
-document.addEventListener("DOMContentLoaded", function () {
-    const darkModeToggle = document.getElementById("dark-mode-toggle");
-    const body = document.body;
-
-    darkModeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-    });
+  darkModeToggle.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+  });
 });
