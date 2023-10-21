ba493c0b43d52c9322e210d65456d9737abdc336
nelsonazuma <nelsonazumah@gmail.com>
2023-10-21 19:15:33 +0100

    Link Update

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -9,3 +9,36 @@ document.querySelector('footer p').textContent = `© ${currentYear} Nelson Azuma
 
 // Set the last modified date in the footer
 document.querySelector('#lastModified').textContent = `Last modified: ${lastModifiedDate}.`;
+
+
+ // function to fetch and display weather data
+ function getWeather() {
+    const apiKey = '9a749bbbab85984be5f1f1fbc7254110';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=accra&units=metric&appid=' + apiKey;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const weatherInfo = document.getElementById('weather-info');
+  
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+  
+        const html = `
+          <ul>
+            <li>Temperature: ${temperature}°C</li>
+            <li>Weather: ${description}</li>
+            <li><img src="https://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon"></li>
+          </ul>
+        `;
+  
+        weatherInfo.innerHTML = html;
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+      });
+  }
+  
+  // Call getWeather function
+  getWeather();
